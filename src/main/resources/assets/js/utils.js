1766f0981488d00e9f23c695a74d8fb688d8e8bb
abogatyrenko <alexander.bogatyr@gmail.com>
2015-06-24 10:41:43 +0300

    refactoring view files

--- src/main/resources/assets/js/utils.js
+++ src/main/resources/assets/js/utils.js
@@ -2,3 +2,60 @@ function activateMenu(elementId) {
     $('ul li.active').removeClass('active');
     $('#' + elementId).addClass('active');
 }
+
+function initButtons() {
+    $("#clear").click(function (event) {
+        $('#content').text('This is a simple single page application');
+        activateMenu('clear');
+    });
+    $("#department").click(function (event) {
+        $('#content').load('department.html');
+        activateMenu('department');
+    });
+    $("#employee").click(function (event) {
+        $('#content').load('employee.html');
+        activateMenu('employee');
+    });
+}
+
+function initDepartmentTable() {
+    $('#departmentTable').DataTable({
+        "ajax": "../../department",
+        "sAjaxDataProp": "",
+        "columns": [
+            {"data": "id"},
+            {"data": "title"}
+        ]
+    });
+}
+
+function initEmployeeTable() {
+    $('#employeeTable').DataTable({
+        "ajax": "../../employee",
+        "sAjaxDataProp": "",
+        "columns": [
+            {"data": "id"},
+            {"data": "firstName"},
+            {"data": "lastName"},
+            {"data": "salary"},
+            {"data": "birthday"},
+            {"data": "active"},
+            {"data": "department.title"}
+        ],
+        "columnDefs": [
+            {
+                "render": function (data, type, row) {
+                    return data + '$';
+                },
+                "targets": 3
+            },
+            {
+                "render": function (data, type, row) {
+                    var isChecked = data ? 'checked' : '';
+                    return '<input type="checkbox" ' + isChecked + ' disabled>';
+                },
+                "targets": 5
+            }
+        ]
+    });
+}
